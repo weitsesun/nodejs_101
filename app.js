ecf1fb49a9ec41dd6886964e02cabc270c9d9480
Wayne Sun <wtse.sun@gmail.com>
2020-03-12 15:23:10 -0700

    created logger service for logging

---
+++ app.js
@@ -0,0 +1,17 @@
+// function sayHello(name) {
+//   console.log('Hello, ' + name)
+// }
+
+// sayHello('Wayne')
+
+/**
+ * In Node, the global execution content is called "global" (not like in Chrome we have "window")
+ * Module - Every file in Node.js is considered a Module
+ */
+// console.log(module) 
+
+/**
+ * 
+ */
+const logger = require('./logger')
+console.log(logger)
---
+++ logger.js
@@ -0,0 +1,13 @@
+/**
+ * Provide logging as a service
+ */
+
+ let url = 'http://mylogger.io/log';
+
+ function log(message) {
+   // Send a HTTP request
+   console.log(message)
+ }
+
+ module.exports.log = log;
+ module.exports.endPoint = url;
