366c157b4657524a777cb3d8d7897c540aa62add
Wayne Sun <wtse.sun@gmail.com>
2020-03-12 16:33:15 -0700

    learned http module

--- app.js
+++ app.js
@@ -53,5 +53,23 @@
 
 // logger.log('message');
 
+/**
+ * Learn HTTP module
+ */
+const http = require('http');
 
+const server = http.createServer((req, res) => {
+  if(req.url === '/') {
+    res.write('Hello World');
+    res.end();
+  }
+  
+  if(req.url === '/api/courses') {
+    res.write(JSON.stringify([1,2,3]));
+    res.end();
+  }
+}); // it's an event emitter
+
+server.listen(3000);
+console.log('Listening on port 3000...');
 
