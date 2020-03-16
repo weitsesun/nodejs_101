d81d59bb08f22974f67bdbe7e4db9e2fce50b9e0
Wayne Sun <wtse.sun@gmail.com>
2020-03-15 22:44:50 -0700

    commented all the code

--- app.js
+++ app.js
@@ -56,20 +56,19 @@
 /**
  * Learn HTTP module
  */
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req, res) => {
-  if(req.url === '/') {
-    res.write('Hello World');
-    res.end();
-  }
+// const server = http.createServer((req, res) => {
+//   if(req.url === '/') {
+//     res.write('Hello World');
+//     res.end();
+//   }
   
-  if(req.url === '/api/courses') {
-    res.write(JSON.stringify([1,2,3]));
-    res.end();
-  }
-}); // it's an event emitter
+//   if(req.url === '/api/courses') {
+//     res.write(JSON.stringify([1,2,3]));
+//     res.end();
+//   }
+// }); // it's an event emitter
 
-server.listen(3000);
-console.log('Listening on port 3000...');
-
+// server.listen(3000);
+// console.log('Listening on port 3000...');
